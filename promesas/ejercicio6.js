068100f381438cf7436b1220aa03a6ccade3269a
Williams Hernández <hernandw@gmail.com>
2024-02-23 15:17:28 -0300

    add try/catch

--- promesas/ejercicio6.js
+++ promesas/ejercicio6.js
@@ -1,13 +1,19 @@
-let datos = (async function cargarDatos() {
-  let respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
-  let datos = await respuesta.json();
+let datos = async function cargarDatos() {
+  try {
+    let respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
+      let datos = await respuesta.json();
+      
+
+  } catch (e) {
+    console.log(e)
+  }
  
 
 
   
-})()
+}
 
-
+datos()
 //crear una funcion reciba el elemento html
 
 //insertarle de manera dinamica los datos
